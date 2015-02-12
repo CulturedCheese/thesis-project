55a29ec1c1e4bb69b2c97e8dc74ec89655ce0348
kchia <kchia87@gmail.com>
2015-02-12 15:11:56 -0800

    FEAT sets up gulp tasks
    DOC adds gulp.config.js
    
    FEAT adds help, default, vet, and wiredep gulp tasks
    
    FIX elaborates on wiredep pipe
    
    FIX fixes gulp tasks
    
    DOC adds .jscsrc to configure repo code style
    
    FIX enables jscs plugin in gulpfile.js
    
    FEAT modifies .jscsrc & .jshintrc to follow Airbnb style guide
    
    CLEANUP adds npm-debug.log to .gitignore
    
    FIX fixes linting errors in gulp.config.js & gulpfile.js

---
+++ gulp.config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = function() {
+  var config = {
+      /**
+       * Files paths
+       */
+      alljs: [
+          './client/*.js',
+          './*.js'
+      ],
+      client: './client/',
+      index: './client/index.html',
+
+      server: './server/',
+
+      /**
+       * Bower and NPM locations
+       */
+      bower: {
+          json: require('./bower.json'),
+          directory: './client/bower_components/',
+          ignorePath: '../..'
+      },
+      packages: [
+          './package.json',
+          './bower.json'
+      ],
+
+      /**
+       * Node settings
+       */
+      defaultPort: 7203,
+      nodeServer: './server/server.js'
+
+  };
+
+  config.getWiredepDefaultOptions = function() {
+    var options = {
+        bowerJson: config.bower.json,
+        directory: config.bower.directory,
+        ignorePath: config.bower.ignorePath
+    };
+    return options;
+  };
+
+  return config;
+
+};
