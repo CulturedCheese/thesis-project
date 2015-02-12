dff6338babcb67517c6e10f4f2f64417921c17fe
kchia <kchia87@gmail.com>
2015-02-12 15:11:56 -0800

    DOC adds gulp.config.js

---
+++ gulp.config.js
@@ -0,0 +1,53 @@
+module.exports = function() {
+    var client = './client/';
+    var root = './';
+    var server = './server/';
+    var temp = './.tmp/';
+    var config = {
+        /**
+         * Files paths
+         */
+        alljs: [
+            './client/*.js',
+            './server/*.js',
+            './*.js'
+        ],
+        client: client,
+        index: client + 'index.html',
+
+        server: server,
+        temp: temp,
+        /**
+         * Bower and NPM locations
+         */
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..'
+        },
+        packages : [
+            './package.json',
+            './bower.json'
+        ],
+
+        /**
+         * Node settings
+         */
+        defaultPort: 7203,
+        nodeServer: './server/app.js'
+
+    };
+
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
+    };
+
+    return config;
+
+ 
+};
