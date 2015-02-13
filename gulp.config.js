90ee1e97275ce8bf37c8976f57a5f2dcb135883e
kchia <kchia87@gmail.com>
2015-02-12 21:54:58 -0800

    FIX fixes linting errors in gulp.config.js & gulpfile.js

--- gulp.config.js
+++ gulp.config.js
@@ -1,48 +1,49 @@
+'use strict';
+
 module.exports = function() {
-    var config = {
-        /**
-         * Files paths
-         */
-        alljs: [
-            './client/*.js',
-            './*.js'
-        ],
-        client: './client/',
-        index: './client/index.html',
+  var config = {
+      /**
+       * Files paths
+       */
+      alljs: [
+          './client/*.js',
+          './*.js'
+      ],
+      client: './client/',
+      index: './client/index.html',
 
-        server: './server/',
+      server: './server/',
 
-        /**
-         * Bower and NPM locations
-         */
-        bower: {
-            json: require('./bower.json'),
-            directory: './client/bower_components/',
-            ignorePath: '../..'
-        },
-        packages : [
-            './package.json',
-            './bower.json'
-        ],
+      /**
+       * Bower and NPM locations
+       */
+      bower: {
+          json: require('./bower.json'),
+          directory: './client/bower_components/',
+          ignorePath: '../..'
+      },
+      packages: [
+          './package.json',
+          './bower.json'
+      ],
 
-        /**
-         * Node settings
-         */
-        defaultPort: 7203,
-        nodeServer: './server/server.js'
+      /**
+       * Node settings
+       */
+      defaultPort: 7203,
+      nodeServer: './server/server.js'
 
+  };
+
+  config.getWiredepDefaultOptions = function() {
+    var options = {
+        bowerJson: config.bower.json,
+        directory: config.bower.directory,
+        ignorePath: config.bower.ignorePath
     };
+    return options;
+  };
 
-    config.getWiredepDefaultOptions = function() {
-        var options = {
-            bowerJson: config.bower.json,
-            directory: config.bower.directory,
-            ignorePath: config.bower.ignorePath
-        };
-        return options;
-    };
+  return config;
 
-    return config;
-
- 
 };
--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,32 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var config = require('./gulp.config.js')();
+ /*jshint -W079 */
 var $ = require('gulp-load-plugins')({lazy: true});
-var port = process.env.PORT || config.defaultPort;
+// var port = process.env.PORT || config.defaultPort;
 
 // provides a list of all tasks in gulpfile
 gulp.task('help', $.taskListing);
 
 // runs the 'help' task by default
-gulp.task('default',['help']);
+gulp.task('default', ['help']);
 
 // checks code syntax and style with JSHint and JSCS
 gulp.task('vet', function() {
   console.log('Analyzing source with JSHint and JSCS');
 
   return gulp
-      .src(config.alljs) // reads all js files into the stream
-      .pipe($.if(args.verbose, $.print())) // prints all files being piped through the stream 
-      .pipe($.jscs()) // lints code style to enforce style guide
-      .pipe($.jshint()) 
-      .pipe($.jshint.reporter('jshint-stylish', {verbose: true})) // adds formatting to the jshint log
-      .pipe($.jshint.reporter('fail')); // outputs 'fail' to the console
+      // reads all js files into the stream
+      .src(config.alljs)
+      // prints all files being piped through the stream
+      .pipe($.if(args.verbose, $.print()))
+      // lints code style to enforce style guide
+      .pipe($.jscs())
+      .pipe($.jshint())
+      // adds formatting to the jshint log
+      .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+      // outputs 'fail' to the console
+      .pipe($.jshint.reporter('fail'));
 });
 
 // injects dependencies into HTML
@@ -30,9 +36,12 @@ gulp.task('wiredep', function() {
   var options = config.getWiredepDefaultOptions();
   var wiredep = require('wiredep').stream;
   return gulp
-      .src(config.index) // reads the index.html file
-      .pipe(wiredep(options)) // checks Bower components and injects into config.index
-      .pipe($.inject(gulp.src(config.alljs))) // takes all config.js files and injects into config.index
-      .pipe(gulp.dest(config.client)); // writes transformed config.index to folder
+      // reads the index.html file
+      .src(config.index)
+      // checks Bower components and injects into config.index
+      .pipe(wiredep(options))
+      // takes all config.js files and injects into config.index
+      .pipe($.inject(gulp.src(config.alljs)))
+      // writes transformed config.index to folder
+      .pipe(gulp.dest(config.client));
 });
-
