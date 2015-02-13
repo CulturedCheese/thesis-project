2f043d394be641981ecacf99c0bd700f75425184
kchia <kchia87@gmail.com>
2015-02-13 12:02:33 -0800

    FEAT configures basic Node/Express server
    FEAT configures server.js
    
    FEAT initializes config files
    
    FEAT adds helper functions
    
    FEAT sets up Express middlewares

---
+++ server/config/middleware.js
@@ -0,0 +1,25 @@
+var morgan      = require('morgan'), // used for logging incoming request
+    bodyParser  = require('body-parser'),
+    helpers     = require('./helpers.js'), // our custom middleware
+
+module.exports = function (app, express) {
+  // Express 4 allows us to use multiple routers with their own configurations
+  // Examples: 
+  // var userRouter = express.Router();
+  // var groupRouter = express.Router();
+
+  app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(express.static(__dirname + '/../client'));
+
+  // TODO: load routers based on request urls
+  // Example: app.use('/api/users', userRouter); // use user router for all user request
+
+  app.use(helpers.errorLogger);
+  app.use(helpers.errorHandler);
+
+  // TODO: inject our routers into their respective route files
+  // Example: 
+  // require('./users/userRoutes.js')(userRouter);
+};
