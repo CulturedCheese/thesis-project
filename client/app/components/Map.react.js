9cba65c27052e4223a54e368b72130e5faa42feb
Todd Skinner <todd.skinner@gmail.com>
2015-02-17 12:26:58 -0800

    FEAT adjusts maps and removes hover feature

--- client/app/components/Map.react.js
+++ client/app/components/Map.react.js
@@ -65,8 +65,8 @@ var GoogleMapFrags = React.createClass({
       <Map
         initialZoom={this.state.zoom}
         initialCenter={this.state.center}
-        width={600}
-        height={400}
+        width={800}
+        height={600}
         onClick={this.handleFaceCreate}>
         {this.state.faces.map(this.renderFace)}
       </Map>
