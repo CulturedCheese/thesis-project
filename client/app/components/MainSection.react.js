b1a6556e3321c5a0029043e79a9703c6186886e2
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-02-19 16:53:56 -0800

    FEAT creates d3Map as a React component with proper Flux data flows from the store

--- client/app/components/MainSection.react.js
+++ client/app/components/MainSection.react.js
@@ -1,45 +1,18 @@
-/**
- * Copyright (c) 2014, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
 var React = require('react');
 var ReactPropTypes = React.PropTypes;
 var DevSearchActions = require('../actions/DevSearchActions');
 var LanguageDropdownMenu = require('./LanguageDropdownMenu.react');
-
-// var TodoItem = require('./TodoItem.react');
+var Map = require('./Map.react');
 
 var MainSection = React.createClass({
 
-  propTypes: {
-    // allTodos: ReactPropTypes.object.isRequired,
-    // areAllComplete: ReactPropTypes.bool.isRequired
-  },
-
-  /**
-   * @return {object}
-   */
   render: function() {
-    console.log('this.props.countryData', this.props.countryData);
-
     return (
       <section id="main">
-        
+        <Map countryData={this.props.countryData} />
       </section>
     );
   },
-
-  /**
-   * Event handler to mark all TODOs as complete
-   */
-  // _onToggleCompleteAll: function() {
-  //   DevSearchActions.toggleCompleteAll();
-  // }
 
 });
 
--- client/app/stores/DevSearchStore.js
+++ client/app/stores/DevSearchStore.js
@@ -19,18 +19,11 @@ var assign = require('object-assign');
 
 var CHANGE_EVENT = 'change';
 
-// var mockData = {};
-
 var mockData = {
   "BRN":{"fillKey":"JavaScript","allLangs":[["JavaScript",3],["Visual Basic",3],["PHP",1]]},
   "THA":{"fillKey":"JavaScript","allLangs":[["JavaScript",2574],["Python",1108],["PHP",922],["Java",862],["CSS",717],["Ruby",686],["Objective-C",583],["C",356],["Shell",306],["Go",295],["C++",253],["Swift",146],["CoffeeScript",138],["C#",115],["Logos",95],["VimL",85],["Scala",84],["Perl",51],["Clojure",45],["Emacs Lisp",43],["Makefile",36],["Lua",29],["Bison",28],["Puppet",27],["R",26],["TeX",25],["Arduino",18],["Elixir",18],["Common Lisp",16],["Objective-C++",14],["PowerShell",13],["Erlang",11],["Haskell",9],["Haxe",9],["Rust",9],["IDL",8],["XSLT",8],["TypeScript",6],["Frege",5],["Groovy",5],["Vala",5],["Hy",4],["VCL",4],["Assembly",2],["F#",2],["Matlab",2],["Visual Basic",2],["Elm",1],["Nimrod",1],["Racket",1]]},
   "USA":{"fillKey":"JavaScript","allLangs":[["JavaScript",247330],["Python",91253],["Ruby",89597],["CSS",67692],["Java",56016],["Go",38898],["PHP",35065],["C",33732],["Objective-C",31845],["C++",31777],["Shell",29214],["C#",16234],["CoffeeScript",14271],["VimL",10766],["R",10087],["Swift",9274],["Scala",7916],["Clojure",7344],["Rust",5872],["Perl",5562],["Haskell",5128],["Emacs Lisp",5109],["TeX",3596],["Lua",2766],["Makefile",2453],["Julia",2055],["Erlang",1958],["Groovy",1784],["Bison",1666],["Arduino",1658],["Puppet",1616],["Matlab",1470],["PowerShell",1357],["OCaml",1336],["TypeScript",1293],["Elixir",1232]]}
 };
-
-
-
-
-
 
 /**
  * Create a TODO item.
