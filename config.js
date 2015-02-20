df0fc4994c4125e635c08288b58bcb7e85b286e0
Todd Skinner <todd.skinner@gmail.com>
2015-02-20 15:50:19 -0800

    FEAT implements oauth1 authentication and oDesk API now returns data to codertracks
    FEAT implements Odesk authentication
    
    FIX attempts to fix search boxes in index.html
    
    CLEAN fixes for rebase
    
    FIX adds index fix
    
    FEAT adds eLance API key and elance logic
    
    FEAT adds to elance related server
    
    Adds Passport for Elance and Odesk
    
    FIX adds use of apiRouter to our API requests
    
    Adds jquery buttons for APIs
    
    FIX fixes index for git
    
    FIX re-adds jquery buttons
    
    FIX adds passport module
    
    FEAT includes work-in-progress for oauth2
    
    FIX fixes url for elance and odesk authorization
    
    FEAT installs express-session and passport-odesk modules
    
    FEAT adds passport and express-session code
    
    FIX disables db connection, as it causes node to crash
    
    FEAT sets up OdeskStrategy
    
    FIX attempts to fix callback URL error
    
    FIX fixes odesk callback URL
    
    FEAT installs cookie-parser dependency
    
    FEAT adds passport authentication routes
    
    FEAT adds cookie-parser middleware
    
    FEAT adds passport serialization and returns odesk user profile
    
    FEAT adds a roles button
    
    FIX passport authentication is working
    
    FIX oDesk API now returns data to codertracks
    
    FIX deletes passport modules and middleware code
    
    CLEANUP removes all eLance API  references
    
    CLEANUP orders modules alphabetically and deletes unneeded comments
    
    CLEANUP configures oDesk API and deletes unneeded comments
    
    CLEANUP formats config's keys to non-string
    
    CLEANUP deletes odesk authorization code sample

---
+++ config.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var config = {};
+
+config = {
+	odeskAPI: 'a9afa7abab1747913f87e577bb45f11f',
+	odeskSECRET: 'f1599d87331c5fcc',
+	odeskAccessToken: '375d1f453e965b258495afecc4a5194d',
+	odeskAccessSecret: 'e2275cea1490a887',
+ 	odeskCallbackURL: 'http://localhost:3000/api/1/auth/odesk/callback'
+};
+
+module.exports = config
