dbfd77e7b79736219ee42f3367f2704593f1cdbf
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-02-20 22:58:22 -0800

    FEAT uses Flux architecture to pass data from server through to the Map

--- client/app/components/DevSearchApp.react.js
+++ client/app/components/DevSearchApp.react.js
@@ -4,33 +4,41 @@ var MainSection = require('./MainSection.react');
 var Map = require('./Map.react');
 var React = require('react');
 var DevSearchStore = require('../stores/DevSearchStore');
-/**
- * This component operates as a "Controller-View".  It listens for changes in
- * the DevSearchStore and passes the new data to its children.
- */
+//This component operates as a "Controller-View".  It listens for changes in
+//the DevSearchStore and passes the new data to its children.
 
-//get the countryData from the store
+//this gets the country data **as it already exists in the store** 
+//this does not make a new api call to the server, only to the store.
 function getDevSearchState() {
-  var countryData = DevSearchStore.getMockData();
+  var countryData = DevSearchStore.getCountryDataFromStore();
   return {
     countryData: countryData
   };
 }
 
+//this tells the store to make an api request for new data from the server
+function requestCountryData() {
+  DevSearchStore.getCountryDataFromServer();
+}
+
 var DevSearchApp = React.createClass({
 
   getInitialState: function() {
+    requestCountryData();
     return getDevSearchState();
   },
 
-  //the component mounting/unmounting functions are purely placeholders. 
-  // componentDidMount: function() {
-  //   DevSearchStore.addChangeListener(this._onChange);
-  // },
+  componentDidMount: function() {
+    DevSearchStore.addChangeListener(this._onChange);
+  },
 
-  // componentWillUnmount: function() {
-  //   DevSearchStore.removeChangeListener(this._onChange);
-  // },
+  _onChange: function() {
+    this.setState(getDevSearchState());
+    //We have to force it to render after updating the state to make sure to pass the new data down to the sub components. 
+    console.log('state after _onChange');
+    console.log(this.state);
+    this.render();
+  },
 
   render: function() {
   	return (
@@ -41,12 +49,6 @@ var DevSearchApp = React.createClass({
       </div>
   	);
   },
-
-  //Event handler for 'change' events coming from the DevSearchStore
-  //We are not sure that this works yet- it's just legacy code right now.
-  _onChange: function() {
-    this.setState(getDevSearchState());
-  }
 
 });
 
--- client/app/stores/DevSearchStore.js
+++ client/app/stores/DevSearchStore.js
@@ -2,11 +2,10 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var EventEmitter = require('events').EventEmitter;
 var DevSearchConstants = require('../constants/DevSearchConstants');
 var assign = require('object-assign');
-//require server connections??
 
 var CHANGE_EVENT = 'change';
 
-var mockData = {
+var countryData = {
   "BRN":{"fillKey":"JavaScript","allLangs":[["JavaScript",3],["Visual Basic",3],["PHP",1]]},
   "THA":{"fillKey":"JavaScript","allLangs":[["JavaScript",2574],["Python",1108],["PHP",922],["Java",862],["CSS",717],["Ruby",686],["Objective-C",583],["C",356],["Shell",306],["Go",295],["C++",253],["Swift",146],["CoffeeScript",138],["C#",115],["Logos",95],["VimL",85],["Scala",84],["Perl",51],["Clojure",45],["Emacs Lisp",43],["Makefile",36],["Lua",29],["Bison",28],["Puppet",27],["R",26],["TeX",25],["Arduino",18],["Elixir",18],["Common Lisp",16],["Objective-C++",14],["PowerShell",13],["Erlang",11],["Haskell",9],["Haxe",9],["Rust",9],["IDL",8],["XSLT",8],["TypeScript",6],["Frege",5],["Groovy",5],["Vala",5],["Hy",4],["VCL",4],["Assembly",2],["F#",2],["Matlab",2],["Visual Basic",2],["Elm",1],["Nimrod",1],["Racket",1]]},
   "USA":{"fillKey":"JavaScript","allLangs":[["JavaScript",247330],["Python",91253],["Ruby",89597],["CSS",67692],["Java",56016],["Go",38898],["PHP",35065],["C",33732],["Objective-C",31845],["C++",31777],["Shell",29214],["C#",16234],["CoffeeScript",14271],["VimL",10766],["R",10087],["Swift",9274],["Scala",7916],["Clojure",7344],["Rust",5872],["Perl",5562],["Haskell",5128],["Emacs Lisp",5109],["TeX",3596],["Lua",2766],["Makefile",2453],["Julia",2055],["Erlang",1958],["Groovy",1784],["Bison",1666],["Arduino",1658],["Puppet",1616],["Matlab",1470],["PowerShell",1357],["OCaml",1336],["TypeScript",1293],["Elixir",1232]]}
@@ -26,9 +25,21 @@ function updateAll(updates) {
 }
 
 var DevSearchStore = assign({}, EventEmitter.prototype, {
-  getCountryData: function() {
-    //ajax request to the server
-    //server returns a json of all the countries, much like our mock data above.
+  //ajax request to the server
+  //server returns a json of all the countries, much like our mock data above.
+  getCountryDataFromServer: function() {
+    $.ajax({
+      url: 'api/1/allCountriesAllLanguages',
+      dataType: 'json',
+      type: 'GET',
+      success: function(data) {
+        countryData = data;
+        this.emitChange();
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error('api/1/getAllFiles', status, err.toString());
+      }.bind(this)
+    });
   },
 
   getLanguageData: function(language) {
@@ -49,8 +60,8 @@ var DevSearchStore = assign({}, EventEmitter.prototype, {
     //make api calls to get the oDesk data
   },
 
-  getMockData: function() {
-    return mockData;
+  getCountryDataFromStore: function() {
+    return countryData;
   },
 
   emitChange: function() {
