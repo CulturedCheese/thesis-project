5bb988a9094a046a1d94149c384b2f0376e37c4c
kchia <kchia87@gmail.com>
2015-02-21 18:42:49 -0800

    FEAT adds passport authentication routes

--- server/api/1/api-router.js
+++ server/api/1/api-router.js
@@ -4,6 +4,12 @@ var databaseLogic = require('./databaseLogic.js');
 var freelancersLogic = require('./freelancersLogic.js');
 var passportElance = require('../../../server/elanceOauth.js');
 var passportOdesk = require('../../../server/odeskOauth.js');
+var Roles = require('odesk-api/lib/routers/hr/roles.js').Roles;
+var oDeskApi = require('odesk-api'), rl = require('readline');
+var creds = require('../../../config.js');
+var api = new oDeskApi({ consumerKey: creds.odeskAPI,
+    consumerSecret: creds.odeskSECRET });
+
 
 apiRouter.get('/allCountriesAllLanguages', function(req,res) {
 	console.log('heard a request to allCountriesAllLanguages');
@@ -25,10 +31,14 @@ apiRouter.get('/odeskData', function(req,res) {
    freelancersLogic.getOdeskData(req,res);
 });
 
+apiRouter.get('/', function(req, res) {
+  console.log(req.user);
+  res.render('about.html');
+});
 // Redirect the user to the OAuth 2.0 provider for authentication.  When
 // complete, the provider will redirect the user back to the application at
 //     /auth/provider/callback
-apiRouter.get('/auth/elance', passportElance.authenticate('elance'));
+// apiRouter.get('/auth/elance', passportElance.authenticate('elance'));
 apiRouter.get('/auth/odesk', passportOdesk.authenticate('odesk'));
 
 // The OAuth 2.0 provider has redirected the user back to the application.
@@ -39,14 +49,17 @@ apiRouter.get('/auth/odesk', passportOdesk.authenticate('odesk'));
 //   passportElance.authenticate('elance', { successRedirect: '/about',
 //                                       failureRedirect: '/contact' }));
 apiRouter.get('/auth/odesk/callback', 
-  passportOdesk.authenticate('odesk', { failureRedirect: '/' }),
+  passportOdesk.authenticate('odesk', {failureRedirect: 'http://www.google.com'}),
   function(req, res){
     console.log('authenticated');
-    res.redirect('/');
+    res.redirect('http://www.yahoo.com');
   });
 
-apiRouter.get('/', function(req, res) {
-  res.render('index');
+apiRouter.get('/roles', function(req, res) {
+   var roles = new Roles(api);
+   roles.getAll(function(error, data){
+     console.log(data);
+   })
 });
 
 module.exports = apiRouter;
