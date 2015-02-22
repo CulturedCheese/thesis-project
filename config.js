9bb52524227dcf338640282f33145fbd298a24eb
kchia <kchia87@gmail.com>
2015-02-21 18:39:52 -0800

    FIX fixes odesk callback URL

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
  	elanceCallbackURL: 'http://localhost:3000/auth/elance/callback',
 	odeskAPI: 'a9afa7abab1747913f87e577bb45f11f',
 	odeskSECRET: 'f1599d87331c5fcc',
- 	odeskCallbackURL: 'http://localhost:3000/auth/odesk/callback'
+ 	odeskCallbackURL: 'http://localhost:3000/api/1/auth/odesk/callback'
 };
 
 // exports.ELANCE_API_KEY = '54e58619e4b0ce56b5a32ecd';
