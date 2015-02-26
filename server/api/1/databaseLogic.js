7d8d70c171e5edc1f1239a135ed8f92ef1e79a90
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-02-26 11:59:39 -0800

    FEAT server functionality for yearly growth, total user count by country and language, mock data in client api endpoints
    FEAT adds in yearly growth and total user count by country server and database functionality
    
    FEAT adds mock data to client endpoints, FIX fixes renaming of a variable in allCountriesAllData endpoint

--- server/api/1/databaseLogic.js
+++ server/api/1/databaseLogic.js
@@ -9,58 +9,84 @@ module.exports = {
     console.log('heard a request to allCountriesAllLanguages');
     //selects the top 10 languages by country;
     var sqlQuery =  'SELECT repository_language, countryCode, activeProgrammers       FROM ( SELECT repository_language, countryCode, activeProgrammers,   @country_rank := IF(@current_country = countryCode, @country_rank + 1, 1) AS country_rank, @current_country := countryCode     FROM 14countries     ORDER BY countryCode, activeProgrammers DESC   ) ranked      WHERE country_rank <= 10';
-    // var sqlQuery2 = 'select * from countriesAggAll limit 100';
-    db.query(sqlQuery, function(err, response) {
-      if(err) {
-        console.error(err);
-      } else {
-        console.log('got data back from topLangsByCountry');
-        //response is an array of objects. 
-        //sample object: {"repository_language":"JavaScript","countryCode":"AM","totalActiveRepos":68}
-        var countries1 = {};
-        for(var i = 0; i < response.length; i++) {
-          var item = response[i];
-          var tuple = [item.repository_language, item.totalActiveRepos];
-          if(!countries1[item.countryCode]) {
-            countries1[item.countryCode] = {
-              fillKey: item.repository_language,
-              allLangs: [tuple]
-            };
-          } else {
-            countries1[item.countryCode].allLangs.push(tuple);
-          }
-        }
-        var countries2 = {};
-        for(var country in countries1) {
-          if(country !== 'null') {
-            var lookupResults = lookup.countries({alpha2: country});
-            if(lookupResults[0]) {
-              var threeLetterName = lookupResults[0].alpha3;
-              
-            }
-            countries2[threeLetterName] = countries1[country];
-            
-          }
-        }
-        res.send(countries2);
-        
+    
+    var sqlQuery2 = 'select * from yoyGrowth';
+
+    db.query(sqlQuery2, function(err, response) {
+      //this outer query gets the list of programmers for each country for each year
+      var yoyGrowth = {};
+
+      //gets all our country data into a pojo for easier processing later.
+      for(var k = 0; k < response.length; k++) {
+        var countryCode = response[k].countryCode;
+        var programmer2013 = response[k].programmers2013;
+        var programmer2014 = response[k].programmers2014;
+        yoyGrowth[countryCode] = [programmer2013,programmer2014];
       }
 
-    }); //this ends the db.query();
+      db.query(sqlQuery, function(err, response) {
+        if(err) {
+          console.error(err);
+        } else {
+          console.log('got data back from topLangsByCountry');
+          console.log(response);
+          //response is an array of objects. 
+          //sample object: {"repository_language":"JavaScript","countryCode":"AM","totalActiveRepos":68}
+          var countries1 = {};
+          for(var i = 0; i < response.length; i++) {
+            var item = response[i];
+            var tuple = [item.repository_language, item.activeProgrammers];
+            if(!countries1[item.countryCode]) {
+              countries1[item.countryCode] = {
+                fillKey: item.repository_language,
+                allLangs: [tuple]
+              };
+            } else {
+              countries1[item.countryCode].allLangs.push(tuple);
+            }
+          }
+          var countries2 = {};
+          for(var country in countries1) {
+            if(country !== 'null') {
+              var lookupResults = lookup.countries({alpha2: country});
+              if(lookupResults[0]) {
+                var threeLetterName = lookupResults[0].alpha3;
+                var countryName = lookupResults[0].name;
+                
+              }
+              countries2[threeLetterName] = countries1[country];
+              countries2[threeLetterName]['countryCode3'] = threeLetterName;
+              countries2[threeLetterName]['countryCode2'] = country;
+              countries2[threeLetterName]['countryName'] = countryName;
+              if(yoyGrowth[country]) {
+                countries2[threeLetterName]['2013'] = yoyGrowth[country][0];
+                countries2[threeLetterName]['2014'] = yoyGrowth[country][1];
+              } else {
+                countries2[threeLetterName]['2013'] = 1;
+                countries2[threeLetterName]['2014'] = 1;
+              }
+              
+            }
+          }
+          res.send(countries2);
+          
+        }
+      }); //this ends the inner db.query();
+      
+    })
+
 
   },
 
   countriesForLanguage: function(req,res) {
     //TODO: figure out what format our language variable is coming in as
-    var languageVar = req.body.language || 'Javascript';
+    var languageVar = req._parsedUrl.query;
     var countriesQuery = 'SELECT activeProgrammers, countryCode FROM 14countries WHERE repository_language="' + languageVar + '" GROUP BY countryCode';
-    var countriesQuery2 = "select countryCode, activeProgrammers FROM 14countries WHERE repository_language='javascript' GROUP BY countryCode";
-    db.query(countriesQuery2, function(err, response) {
+    // var countriesQuery2 = "select countryCode, activeProgrammers FROM 14countries WHERE repository_language='javascript' GROUP BY countryCode";
+    db.query(countriesQuery, function(err, response) {
       if(err) {
         console.error(err);
       } else {
-        console.log('response!');
-        console.log(response);
 
         //all this logic is to get the three letter country code, and to format our response object to use the fillKey format
         var countries = {};
@@ -75,6 +101,28 @@ module.exports = {
           }
         }
         res.send(countries);
+      }
+    });
+  },
+
+  developerCountByCountry: function(req,res) {
+    var sqlQuery = 'SELECT * FROM yoyGrowth';
+    db.query(sqlQuery, function(err, response) {
+      if(err) {
+        console.error(err);
+      } else {
+        res.send(response);
+      }
+    });
+  },
+
+  developerCountByLanguage: function(req,res) {
+    var sqlQuery = 'SELECT * FROM languages';
+    db.query(sqlQuery, function(err, response) {
+      if(err) {
+        console.error(err);
+      } else {
+        res.send(response);
       }
     });
   }
