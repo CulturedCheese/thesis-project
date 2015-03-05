cb542fd56be6d18831f6fbba743b4fa70fdc5485
Todd Skinner <todd.skinner@gmail.com>
2015-03-04 22:58:59 -0800

    FEAT adds working inital map and initialBox
    FEAT adds colored initial map
    
    FEAT adds dominant lang on hover
    
    FEAT adds info to initialBox

---
+++ client/app/components/InitialSubBox.react.js
@@ -0,0 +1,31 @@
+var React = require('react');
+
+var InitialSubBox = React.createClass({
+
+  numberWithCommas: function(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  },
+
+  render: function() {
+    var result = this.props.initialWorkflowData;   
+    return (
+      <div className="initialsubbox">
+        <h5><u>Dominant Language     </u><u># Devs </u> </h5> 
+        <ol>
+          <li> { result["topTenLangs"][0][0] } ({ result["topTenLangs"][0][1] })  </li>
+          <li> { result["topTenLangs"][1][0] } ({ result["topTenLangs"][1][1] })  </li>
+          <li> { result["topTenLangs"][2][0] } ({ result["topTenLangs"][2][1] })  </li>
+          <li> { result["topTenLangs"][3][0] } ({ result["topTenLangs"][3][1] })  </li>
+          <li> { result["topTenLangs"][4][0] } ({ result["topTenLangs"][4][1] })  </li>
+          <li> { result["topTenLangs"][5][0] } ({ result["topTenLangs"][5][1] })  </li>
+          <li> { result["topTenLangs"][6][0] } ({ result["topTenLangs"][6][1] })  </li>
+          <li> { result["topTenLangs"][7][0] } ({ result["topTenLangs"][7][1] })  </li>
+          <li> { result["topTenLangs"][8][0] } ({ result["topTenLangs"][8][1] })  </li>                    <li> { result["topTenLangs"][0][0] } ({ result["topTenLangs"][0][1] })  </li>
+          <li> { result["topTenLangs"][9][0] } ({ result["topTenLangs"][9][1] })  </li>
+        </ol>
+      </div>
+    );
+  }
+});
+
+module.exports = InitialSubBox;
